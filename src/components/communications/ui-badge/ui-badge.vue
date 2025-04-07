def0048bf68fb5e0290fdfdc62f5af686a88fc2b
BruhaBruh <drugsho.jaker@gmail.com>
2025-04-07 22:17:45 +0500

    feat: improve badge animations

--- src/components/communications/ui-badge/ui-badge.vue
+++ src/components/communications/ui-badge/ui-badge.vue
@@ -3,6 +3,8 @@ import type { PropsPolymorphic } from '@/types';
 import type { VariantProps } from 'class-variance-authority';
 import { computed } from 'vue';
 import { badgeVariants } from './ui-badge.variants';
+import { AnimatePresence, motion } from 'motion-v';
+import { materialDuration, materialEasing } from '@/config';
 
 type Variants = VariantProps<typeof badgeVariants>;
 
@@ -18,32 +20,58 @@ const {
   placement,
   value = 0,
   maxValue = 999,
-  as = 'span',
+  as = 'div',
 } = defineProps<BadgeProps>();
 
 const displayValue = computed(() => {
   if (value < 1) return '';
   if (value > maxValue) return `${maxValue}+`;
-  return value;
+  return `${value}`;
 });
 </script>
 
 <template>
   <component :is="as" class="relative" v-tw-merge>
     <slot />
-    <span
-      v-if="value >= 0"
-      :data-one-digit="value < 10"
-      :class="
-        badgeVariants({
-          size: value < 1 ? 'small' : 'large',
-          color,
-          placement,
-        })
-      "
-      v-tw-merge
-    >
-      {{ displayValue }}
-    </span>
+    <AnimatePresence mode="wait">
+      <motion.span
+        v-if="value >= 0"
+        :data-one-digit="value < 10"
+        :initial="{ opacity: 0, scale: 0 }"
+        :animate="{
+          opacity: 1,
+          scale: 1,
+        }"
+        :exit="{ opacity: 0, scale: 0 }"
+        :transition="{
+          duration: materialDuration.asMotion('medium-1'),
+          ease: materialEasing.standard,
+        }"
+        :class="
+          badgeVariants({
+            size: value < 1 ? 'small' : 'large',
+            color,
+            placement,
+          })
+        "
+        v-tw-merge
+      >
+        <AnimatePresence mode="popLayout">
+          <motion.span
+            v-for="(char, i) in displayValue.split('')"
+            :key="char + i"
+            :initial="{ y: -20, opacity: 0 }"
+            :animate="{ y: 0, opacity: 1 }"
+            :exit="{ y: 20, opacity: 0 }"
+            :transition="{
+              duration: materialDuration.asMotion('short-2'),
+              ease: materialEasing.standard,
+            }"
+          >
+            {{ char }}
+          </motion.span>
+        </AnimatePresence>
+      </motion.span>
+    </AnimatePresence>
   </component>
 </template>
